83ac7338a3c44f46ee8f0b7f7438958179a61c4d
nyannncatt <kamirelax187@gmail.com>
2024-02-14 23:35:25 +0800

    added json data

--- app.js
+++ app.js
@@ -13,44 +13,14 @@ closeShopping.addEventListener('click', ()=>{
     body.classList.remove('active');
 })
 
-let products = [
-    {
-        id: 1,
-        name: 'PRODUCT NAME 1',
-        image: '1.PNG',
-        price: 120000
-    },
-    {
-        id: 2,
-        name: 'PRODUCT NAME 2',
-        image: '2.PNG',
-        price: 120000
-    },
-    {
-        id: 3,
-        name: 'PRODUCT NAME 3',
-        image: '3.PNG',
-        price: 220000
-    },
-    {
-        id: 4,
-        name: 'PRODUCT NAME 4',
-        image: '4.PNG',
-        price: 123000
-    },
-    {
-        id: 5,
-        name: 'PRODUCT NAME 5',
-        image: '5.PNG',
-        price: 320000
-    },
-    {
-        id: 6,
-        name: 'PRODUCT NAME 6',
-        image: '6.PNG',
-        price: 120000
-    }
-];
+let products = [];
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    products = data;
+    initApp(); // Call initApp after fetching data
+  })
+  .catch(error => console.error('Error fetching product data:', error));
 let listCards  = [];
 function initApp(){
     products.forEach((value, key) =>{
